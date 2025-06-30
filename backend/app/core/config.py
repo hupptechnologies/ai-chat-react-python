f3ecf111a914515dbfe32a9594a5b343ff12fe9e
hupptechnologies <git@hupp.in>
2025-06-30 18:23:09 +0530

    [BE][update] - Update requirements file remove unused packages and add all the package versions

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite+aiosqlite:///./test.db"
+    DATABASE_URL: str = ""
     OPENAI_API_KEY: str = ""
     allowed_origins: list = ["http://localhost:3000"]
 
